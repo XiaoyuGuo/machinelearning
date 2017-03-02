cd17eba658fd553e0998151c68808b8ec81be404
XiaoyuGuo <16126144@bjtu.edu.cn>
2017-03-02 23:22:54 +0800

    Notes

---
+++ maxentropy.py
@@ -0,0 +1,4 @@
+# Max Entropy Model
+# Author  : Guoxiaoyu
+# Date    : 2017/3/2
+# Version : Python3.6.0
---
+++ svm.py
@@ -0,0 +1,4 @@
+# Support Vector Machine
+# Author  : Guoxiaoyu
+# Date    : 2017/3/2
+# Version : Python3.6.0

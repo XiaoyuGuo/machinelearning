82abd2b0cd94fdaaa74cefd715a8f01ec7e97afa
XiaoyuGuo <16126144@bjtu.edu.cn>
2017-03-03 01:16:45 +0800

    123

--- maxentropy.py
+++
@@ -1,4 +0,0 @@
-# Max Entropy Model
-# Author  : Guoxiaoyu
-# Date    : 2017/3/2
-# Version : Python3.6.0
--- svm.py
+++ svm.py
@@ -2,3 +2,17 @@
 # Author  : Guoxiaoyu
 # Date    : 2017/3/2
 # Version : Python3.6.0
+
+class LabeledPoint():
+    '''Labeled point'''
+    def __init__(self, point=None, label=None):
+        '''Construct func'''
+        self.point = point
+        self.label = label
+
+class SVMModel():
+    '''SVMModel'''
+    def train(self):
+        pass
+    def predict(self):
+        pass
